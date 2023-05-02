11347df402dbff74052082d62504e41b08617b29
erenvxl <aguelci1@uni-koeln.de>
2023-05-02 16:10:36 +0200

    Implemented tailwindcss framework, + starter/intro page

--- src/lib/Starter.svelte
+++ src/lib/Starter.svelte
@@ -1,12 +1,39 @@
 <script>
+    import zfl from "$lib/assets/img/zfl-logo.png"
+    import starter from "$lib/assets/img/stock-img/starter.jpg"
 </script>
 
-<section id="starter">
+<section id="starter" class="grid grid-cols-3" style="height: 100vh">
+    <div style="height: 100vh; position: relative" class="bg-black col-span-1">
+        <span class="text-white text-2xl block p-20 justify-center ">
+                Jedes Jahr verlassen
+                Zehntausende junge Menschen
+                die Schule ohne Abschluss.
+                Die Zahl der Ungelernten
+                steigt, ihre Jobchancen sinken.
+                Dabei würden sie auf dem
+                Arbeitsmarkt dringend
+                gebraucht. Die Weichen für
+                den Bildungsweg werden
+                schon früh gestellt.
+            </span>
+
+        <img src="{zfl}" alt="ZfL-Logo" style="position: absolute; height: 15%; bottom: 1rem; left: 1rem" >
+    </div>
+    <div style="height: 100vh" class="col-span-2">
+        <img src="{starter}" alt="Starter Image" class="overflow-y-auto" style="height: 100%; object-fit: cover">
+    </div>
 </section>
 
-<style>
+<section id="page-2">
+</section>
+
+<section id="page-3">
+</section>
+
+
+<style lang="postcss">
     #starter {
-        height: 100vh;
         background-color: #0078a8;
     }
 </style>
