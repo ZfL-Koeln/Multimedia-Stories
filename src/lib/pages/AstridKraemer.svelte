1324214db5bca96d58d1cd28a56878a832da2fbc
Jan Wieners <mail@jan-wieners.de>
2023-07-12 20:58:07 +0200

    Change button text, adjust styling of youtube overlays.

--- src/lib/pages/AstridKraemer.svelte
+++ src/lib/pages/AstridKraemer.svelte
@@ -13,9 +13,9 @@
         	<iframe class="aspect-video w-full lg:mt-8" src="https://www.youtube-nocookie.com/embed/mZpcDfHXGhw" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         	-->
             <div class="backdrop-blur-md backdrop-brightness-75 hover:backdrop-brightness-50 absolute inset-0 flex flex-col justify-center items-center {acceptYoutube ? 'hidden' : 'block'} transition-all">
-                <p class="font-roboto select-none text-white text-lg md:text-3xl text-center pb-4">{youtubeMessage}</p>
+                <p class="font-roboto select-none text-white text-lg md:text-2xl text-center pb-4 md:px-10">{youtubeMessage}</p>
                 <button class="border-2 drop-shadow-xl text-lg rounded-full border-white font-sans text-white py-2 px-4 bg-white bg-opacity-0 hover:bg-opacity-30 active:bg-opacity-50 transition-all duration-300"
-                        on:click={() => acceptYoutube = true}> Akzeptieren
+                        on:click={() => acceptYoutube = true}> Ok
                 </button>
             </div>
         </div>
