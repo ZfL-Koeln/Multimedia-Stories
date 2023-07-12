b2d88fc04f12191aee5a2265661a2170b1b7bc38
Sascha <sbaumga4@uni-koeln.de>
2023-07-12 15:16:35 +0200

    merge conflict fixed.

--- src/lib/pages/DanielKeil.svelte
+++ src/lib/pages/DanielKeil.svelte
@@ -12,8 +12,6 @@
 		<div class="relative">
 			<iframe class="aspect-video w-full" src="https://www.youtube-nocookie.com/embed/_R8v5aw6vUA" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 			<div class="backdrop-blur-md backdrop-brightness-75 hover:backdrop-brightness-50 absolute inset-0 flex flex-col justify-center items-center {acceptYoutube ? 'hidden' : 'block'} transition-all">
-				<p class="font-roboto select-none text-white md:text-2xl text-center pb-4 px-10">{youtubeMessage}</p>
-				<button class="border-2 drop-shadow-xl text-lg rounded-full border-white font-sans text-white py-2 px-4 bg-white bg-opacity-0 hover:bg-opacity-30 active:bg-opacity-50 transition-all duration-300 sm:px-1" on:click={() => acceptYoutube = true}> Akzeptieren</button>
 				<p class="font-roboto select-none text-white text-lg md:text-3xl text-center pb-4">{youtubeMessage}</p>
 				<button class="border-2 drop-shadow-xl text-xl rounded-full border-white font-sans text-white py-2 px-4 bg-white bg-opacity-0 hover:bg-opacity-30 active:bg-opacity-50 transition-all duration-300" on:click={() => acceptYoutube = true}> Akzeptieren</button>
 			</div>
